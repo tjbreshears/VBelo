95cdaa8aa089ecea6ef379bb34df29c7a86a52e8
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2021-03-08 17:05:52 -1000

    Added Top 10 list via pandas

---
+++ outputs/top_10.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('outputs/teams_ouput')
+rank = df.sort_values('elo',ascending=False).loc[df['tracking'] == 1].head(10)
+rank = rank.drop(['short_name', 'division', 'mascot','conference','tracking'], axis=1)
+rank.columns = ['School', 'Elo']
+rank = rank.to_string(index=False)
+
+
+print(rank)
