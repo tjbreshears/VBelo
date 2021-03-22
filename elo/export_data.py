08b7f23f82bf950b4a8fd428939e46d52d0297dc
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2021-03-21 18:43:38 -1000

    Fixed typo

--- elo/export_data.py
+++ elo/export_data.py
@@ -9,7 +9,7 @@ def export_teams (teams):
 
 def export_games (games):
     field_names = ['date','season','home','neutral','playoff','team1','team2','result_team1','result_team2','sets_team1','sets_team2','points_team1','points_team2','elo_start_team1','elo_start_team2','probability_team1','probability_team2','elo_end_team1','elo_end_team2']
-    with open('outputs/games_2021_ouput', 'w') as csvfile:
+    with open('outputs/games_2021_output', 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames = field_names)
         writer.writeheader()
         writer.writerows(games)
