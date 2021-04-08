98b67dcc3d38a45b0db48933cad24939067022f2
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2021-04-07 16:54:29 -1000

    Home Court Calculation

--- src/export_data.py
+++ src/export_data.py
@@ -8,7 +8,7 @@ def export_teams (teams):
         writer.writerows(teams)
 
 def export_games (games):
-    field_names = ['date','season','home','neutral','playoff','team1','team2','result_team1','result_team2','sets_team1','sets_team2','points_team1','points_team2','elo_start_team1','elo_start_team2','probability_team1','probability_team2','elo_end_team1','elo_end_team2']
+    field_names = ['date','season','home','neutral','playoff','team1','team2','result_team1','result_team2','sets_team1','sets_team2','points_team1','points_team2','elo_start_team1','elo_start_team2','elo_adjusted_team1','elo_adjusted_team2','probability_team1','probability_team2','elo_end_team1','elo_end_team2']
     with open('outputs/games_2021_output.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames = field_names)
         writer.writeheader()
