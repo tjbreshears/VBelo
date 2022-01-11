24c9b703470100462cd218bfc9781ffbec452268
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2022-01-10 18:59:03 -1000

    Added current MSE function

--- src/mse.py
+++ src/mse.py
@@ -4,6 +4,7 @@ import csv
 import numpy as np
 import vbelo
 
+# MSE for K values
 def mse_k ():
     best = [1,1]
     for K in range (0,50):
@@ -30,6 +31,7 @@ def mse_k ():
     print(f'Best K: {best[0]}')
     print(f'Best RSE: {best[1]}')
 
+# MSE for travel variable values
 def mse_t ():
     best = [1,1]
     for t in range (-20,0):
@@ -56,8 +58,7 @@ def mse_t ():
     print(f'Best t: {best[0]}')
     print(f'Best RSE: {best[1]}')
 
-# Looks for lowest MSE for both K and t
-#
+# MSE for K and travel variable values
 def mse_kt ():
     best = [1,1,1]
     for K in range (16,36):
@@ -87,4 +88,28 @@ def mse_kt ():
     print(f'Best t: {best[1]}')
     print(f'Best RSE: {best[2]}')
 
-mse_kt()
+# MSE for one set of input variables
+def mse_current ():
+    vbelo.season (24,-1,'2021')
+    games = []
+    with open("outputs/games_output.csv", 'r') as data:
+        for line in csv.DictReader(data):
+            games.append(line)
+
+    expected = []
+    actual = []
+
+    for i in range(len(games)):
+        if games[i]['result_team1'] != '':
+            expected.append(float(games[i]['probability_team1']))
+            expected.append(float(games[i]['probability_team2']))
+            actual.append(float(games[i]['result_team1']))
+            actual.append(float(games[i]['result_team2']))
+
+    mse = np.square(np.subtract(expected,actual)).mean()
+    print(f'Current RSE: {mse}')
+
+#mse_k()
+#mse_t()
+#mse_kt()
+mse_current()
