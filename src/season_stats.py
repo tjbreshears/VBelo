603ba3335d27e0503191251e2d74ae3cb9aff8c8
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2022-04-04 12:49:13 -1000

    Updated rounding

--- src/season_stats.py
+++ src/season_stats.py
@@ -22,19 +22,19 @@ def season_stats (year):
         if games[i]['season'] == year and games[i]['r_t1'] != '':
             for x in range(len(teams)):
                 if games[i]['t1'] == teams[x]['short_name']:
-                    teams[x]['elo'] = games[i]['elo_end_team1']
+                    teams[x]['elo'] = round(float(games[i]['elo_end_team1']),2)
                     teams[x]['games'] = int(teams[x]['games']) + 1
                     if games[i]['r_t1'] == '1':
                         teams[x]['wins'] = int(teams[x]['wins']) + 1
                     else:
                         teams[x]['losses'] = int(teams[x]['losses']) + 1
-                    teams[x]['win_pct'] = int(teams[x]['wins'])/int(teams[x]['games'])
+                    teams[x]['win_pct'] = round(int(teams[x]['wins'])/int(teams[x]['games']),3)
                     if games[i]['home'] == games[i]['t2'] and games[i]['r_t1'] == '1':
                         teams[x]['away_wins'] = int(teams[x]['away_wins']) + 1
                     if games[i]['home'] == games[i]['t2'] and games[i]['r_t1'] == '0':
                         teams[x]['away_losses'] = int(teams[x]['away_losses']) + 1
                     if int(teams[x]['away_wins'])+int(teams[x]['away_losses']) != 0:
-                        teams[x]['away_pct'] = int(teams[x]['away_wins'])/(int(teams[x]['away_wins'])+int(teams[x]['away_losses']))
+                        teams[x]['away_pct'] = round(int(teams[x]['away_wins'])/(int(teams[x]['away_wins'])+int(teams[x]['away_losses'])),3)
 
                     teams[x]['sets_won'] = int(teams[x]['sets_won']) + int(games[i]['s_t1'])
                     teams[x]['sets_lost'] = int(teams[x]['sets_lost']) + int(games[i]['s_t2'])
@@ -43,21 +43,21 @@ def season_stats (year):
                     teams[x]['points_lost'] = int(teams[x]['points_lost']) + int(games[i]['p_t2'])
                     teams[x]['point_diff'] = teams[x]['points_won'] - teams[x]['points_lost']
                     teams[x]['soo'] = float(teams[x]['soo']) + float(games[i]['elo_start_team2'])
-                    teams[x]['sos'] = teams[x]['soo']/teams[x]['games']
+                    teams[x]['sos'] = round(teams[x]['soo']/teams[x]['games'],2)
                 if games[i]['t2'] == teams[x]['short_name']:
-                    teams[x]['elo'] = games[i]['elo_end_team2']
+                    teams[x]['elo'] = round(float(games[i]['elo_end_team2']),2)
                     teams[x]['games'] = int(teams[x]['games']) + 1
                     if games[i]['r_t2'] == '1':
                         teams[x]['wins'] = int(teams[x]['wins']) + 1
                     else:
                         teams[x]['losses'] = int(teams[x]['losses']) + 1
-                    teams[x]['win_pct'] = int(teams[x]['wins'])/int(teams[x]['games'])
+                    teams[x]['win_pct'] = round(int(teams[x]['wins'])/int(teams[x]['games']),3)
                     if games[i]['home'] == games[i]['t2'] and games[i]['r_t2'] == '1':
                         teams[x]['home_wins'] = int(teams[x]['home_wins']) + 1
                     if games[i]['home'] == games[i]['t2'] and games[i]['r_t2'] == '0':
                         teams[x]['home_losses'] = int(teams[x]['home_losses']) + 1
                     if int(teams[x]['home_losses'])+int(teams[x]['home_wins']) != 0:
-                        teams[x]['home_pct'] = int(teams[x]['home_wins'])/(int(teams[x]['home_losses'])+int(teams[x]['home_wins']))
+                        teams[x]['home_pct'] = round(int(teams[x]['home_wins'])/(int(teams[x]['home_losses'])+int(teams[x]['home_wins'])),3)
 
                     teams[x]['sets_won'] = int(teams[x]['sets_won']) + int(games[i]['s_t2'])
                     teams[x]['sets_lost'] = int(teams[x]['sets_lost']) + int(games[i]['s_t1'])
